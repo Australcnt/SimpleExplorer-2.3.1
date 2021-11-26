24471e9b5f0cc54635d3f36f160154926f95b62b
Australcnt <924690736@qq.com>
2021-11-26 19:47:53 +0800

    newview

---
+++ explorer/src/main/java/com/dnielfe/manager/debug/Newview.java
@@ -0,0 +1,25 @@
+package com.dnielfe.manager.debug;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.dnielfe.manager.R;
+import com.dnielfe.manager.ThemableActivity;
+
+public class Newview extends ThemableActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newview);
+    }
+
+    public void goBackToMain(View view) {
+        this.finish();
+        Intent intent = new Intent(getBaseContext(), com.dnielfe.manager.BrowserActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+}
